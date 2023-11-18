36d0a28cf0d568e9e4382773c44206d87a59442e
David <sebedavid04@gmail.com>
2023-11-19 00:15:48 +0100

    updatee

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/nhf
+# Install script for directory: D:/programok/c/NAGYHAZI
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/nhf/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/programok/c/NAGYHAZI/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
